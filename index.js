65ac0232f6872b790405e4f28b29fdd4dfe28d69
Stacy-JoyM <jstacymuthoka@gmail.com>
2024-10-02 13:23:32 +0300

    Find win

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+function superbowlWin(records){
+    //use find() to see if theres a record with "W"in result
+    const win = records.find(record =>record.result === "W") 
+    if (win){
+        return win.year
+    }
+}
+
+const records= [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+    //...
+]
+console.log(superbowlWin(records))
